ca00650cf3af173ed8f09e2663dd7585fee3a34c
Alex Shelley <ashelley@purdue.edu>
2016-12-19 16:46:14 -0500

    Created BattleUIManager, which will handle GUI callbacks

---
+++ Assets/Scripts/BattleUIManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BattleUIManager : MonoBehaviour
+{
+    public BattlePanel initialPanel;
+
+    private BattlePanel currentPanel;
+    
+    //Events
+
+    void Start()        //Using start instead of awake so initialPanel can rut its Awake first.
+    {
+        //Change to the initial panel
+        currentPanel = initialPanel;
+        currentPanel.Show();
+    }
+
+    //Interface
+
+    public void ChangePanels(BattlePanel targetPanel)
+    {
+        //Hide the old panel
+        currentPanel.Hide();
+
+        //Show the new panel
+        currentPanel = targetPanel;
+        currentPanel.Show();
+    }
+
+    //Misc methods
+}
