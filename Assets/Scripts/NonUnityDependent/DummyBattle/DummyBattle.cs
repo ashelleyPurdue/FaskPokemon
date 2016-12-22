08553399cad9b463aa9df90c15896cc4dea31918
Alex Shelley <ashelley@purdue.edu>
2016-12-21 21:28:10 -0500

    DummyBattle now runs its turn logic in a separate thread, so that it can be 	suspended and resumed at will.

--- Assets/Scripts/NonUnityDependent/DummyBattle/DummyBattle.cs
+++ Assets/Scripts/NonUnityDependent/DummyBattle/DummyBattle.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System.Threading;
+using System.Collections.Generic;
 
 public class DummyBattle
 {
@@ -8,13 +9,19 @@ public class DummyBattle
     public event BasicEventHandler OnBattleStart;       //Called when the battle starts
     public event BasicEventHandler OnTurnEnded;         //Called when the current turn is over and the battle is awaiting the player's next move.
 
+    public delegate void CommandExecutedHanlder(DummyBattleCommand command);
+
+    public event CommandExecutedHanlder OnPlayerCommandExecuted;
+    public event CommandExecutedHanlder OnEnemyCommandExecuted;
 
     //Fields
     public DummyPokemon playerPokemon { get; private set; }
     public DummyPokemon enemyPokemon { get; private set; }
 
+    private bool isThreadRunning = false;
 
-    //Factories
+
+    //Constructors
     public DummyBattle()
     {
         //Start out the dummy pokemon
@@ -26,18 +33,39 @@ public class DummyBattle
     //Interface
     public void DoTurn(DummyBattleCommand playerCommand, DummyBattleCommand enemyCommand)
     {
+        //Throw an exception if the turn thread is already running
+        if (isThreadRunning)
+        {
+            throw new System.Exception("Turn thread already running.  Please wait for it to finish!");
+        }
+
+        //Start the turn thread
+        isThreadRunning = true;
+
+        Thread turnThread = new Thread(DoTurnThread);
+        turnThread.Start(new DoTurnArgs(playerCommand, enemyCommand));
+    }
+
+
+    //Misc methods
+
+    private void DoTurnThread(object args)
+    {
+        //The the parameters from the arg
+        DoTurnArgs turnArgs = (DoTurnArgs)args;
+        DummyBattleCommand playerCommand = turnArgs.playerCommand;
+        DummyBattleCommand enemyCommand = turnArgs.enemyCommand;
+
         //TODO: Decide who goes first
 
         ExecuteCommand(playerCommand);
         ExecuteCommand(enemyCommand);
 
         //Send the event
+        isThreadRunning = false;
         OnTurnEnded.Invoke();
     }
-
-
-    //Misc methods
-
+    
     private void ExecuteCommand(DummyBattleCommand command)
     {
         //If it's a move, use it.
@@ -53,6 +81,20 @@ public class DummyBattle
         {
             case 0: user.Tackle(target); break;
             case 1: user.PoisonSting(target); break;
+        }
+    }
+
+
+    //Nested classes
+    class DoTurnArgs
+    {
+        public DummyBattleCommand playerCommand;
+        public DummyBattleCommand enemyCommand;
+
+        public DoTurnArgs(DummyBattleCommand playerCommand, DummyBattleCommand enemyCommand)
+        {
+            this.playerCommand = playerCommand;
+            this.enemyCommand = enemyCommand;
         }
     }
 }
