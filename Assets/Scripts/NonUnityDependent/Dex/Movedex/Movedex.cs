76e19675ee0b6515feb95de00e988c29ea6abcb6
Alex Shelley <ashelley@purdue.edu>
2016-12-29 14:41:09 -0500

    Created the Movedex.

---
+++ Assets/Scripts/NonUnityDependent/Dex/Movedex/Movedex.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+public static class Movedex
+{
+    private static GenericDex<MovedexEntry> dex;
+
+    public static void AddEntry(MovedexEntry entry)
+    {
+        //Throw an exception if there's a duplicate id
+        dex.AddEntry(entry.id, entry);
+    }
+
+    public static MovedexEntry GetEntry(DexID id)
+    {
+        return dex.GetEntry(id);
+    }
+}
---
+++ Assets/Scripts/NonUnityDependent/Dex/Movedex/MovedexEntry.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+
+public abstract class MovedexEntry
+{
+    public DexID id;
+    public string moveName;
+    public string moveDescription;
+
+    public MoveCategory moveCategory;
+
+
+    //Constructors
+
+    public MovedexEntry(DexID id, string moveName, string moveDescription, MoveCategory moveCategory)
+    {
+        this.id = id;
+        this.moveName = moveName;
+        this.moveDescription = moveDescription;
+
+        this.moveCategory = moveCategory;
+    }
+    
+
+    //Abstract methods
+
+    public abstract void Use(IndividualPokemon user, IndividualPokemon target);
+}
+
+public enum MoveCategory
+{
+    physical,
+    special,
+    status
+}
