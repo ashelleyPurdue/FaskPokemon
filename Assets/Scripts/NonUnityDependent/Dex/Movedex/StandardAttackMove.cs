e4edd656faa12aeae23e976a4a6af64622d5bf5a
Alex Shelley <ashelley@purdue.edu>
2016-12-29 15:42:29 -0500

    Created standard attack moves

---
+++ Assets/Scripts/NonUnityDependent/Dex/Movedex/StandardAttackMove.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+public class StandardAttackMove : MovedexEntry
+{
+    public int power;
+    public int accuracy;
+
+    public StandardAttackMove(DexID id, string moveName, string moveDescription, MoveCategory moveCategory, int basePP, int power, int accuracy)
+        : base(id, moveName, moveDescription, moveCategory, basePP)
+    {
+        this.power = power;
+        this.accuracy = accuracy;
+    }
+
+    public override void Use(IndividualPokemon user, IndividualPokemon target)
+    {
+        //TODO: Deal damage using the damage formula
+        throw new NotImplementedException();
+    }
+}
