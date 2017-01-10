8d26bde640d756bdf6ade66dddcc18a663690cef
Alex Shelley <ashelley@purdue.edu>
2017-01-10 14:37:18 -0500

    The BattlePokemonDisplays now play the test tackle animation at the start. Adjusted the test tackle animation.

--- Assets/Scripts/BattleScene/BattlePokemonDisplay.cs
+++ Assets/Scripts/BattleScene/BattlePokemonDisplay.cs
@@ -4,17 +4,23 @@ using UnityEngine;
 using SimpleAnimationSystem;
 
 [RequireComponent(typeof(FlashEffect))]
+[RequireComponent(typeof(SimpleAnimationPlayer))]
 public class BattlePokemonDisplay : MonoBehaviour
 {
     public IndividualPokemon pokemon { get; private set; }
 
     private FlashEffect flasher;
+	private SimpleAnimationPlayer animPlayer;
 
     //Events
 
     void Awake()
     {
         flasher = GetComponent<FlashEffect>();
+		animPlayer = GetComponent<SimpleAnimationPlayer>();
+
+		//TEST: Play the simple tackle animation
+		animPlayer.PlayAnimation(TestAnimations.tackle);
     }
 
     //Interface
@@ -61,8 +67,8 @@ public static class TestAnimations
 	{
 		//Create the tackle animation
 		List<KeyFrame> frames = new List<KeyFrame>();
-		KeyFrame start = new KeyFrame(0, new Transformation());
-		KeyFrame end = new KeyFrame(0.5f, new Transformation(new Vector3(1, 1), Vector3.one, Quaternion.identity));
+		KeyFrame start = new KeyFrame(0, new Transformation(Vector3.zero, Vector3.one, Quaternion.identity));
+		KeyFrame end = new KeyFrame(0.5f, new Transformation(new Vector3(0.5f, 0.5f), Vector3.one, Quaternion.identity));
 		KeyFrame comeBack = new KeyFrame(1f, start.transformation);
 
 		frames.Add(start);
