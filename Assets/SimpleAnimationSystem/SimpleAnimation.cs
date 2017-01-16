e8f9d1ea824add90d7c9c5196bc79715011ae485
Alex Shelley <ashelley@purdue.edu>
2017-01-16 17:26:18 -0500

    KeyFrames can now be converted to a strings(needs testing). SimpleAnimations can now be created from strings(needs testing).

--- Assets/SimpleAnimationSystem/SimpleAnimation.cs
+++ Assets/SimpleAnimationSystem/SimpleAnimation.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections;
+using System.Text;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,6 +7,11 @@ namespace SimpleAnimationSystem
 {
     public class SimpleAnimation
     {
+		public const string OFFSET_COMMAND = "offset";
+		public const string POS_COMMAND = "pos";
+		public const string SCALE_COMMAND = "scale";
+		public const string EULER_ROT_COMMAND = "rotEuler";
+
         public int numKeyframes { get { return keyframes.Count; } }
         public float animationLen { get; private set; }
 
@@ -20,10 +26,70 @@ namespace SimpleAnimationSystem
             animationLen = CalculateAnimationLength();
         }
 
+		public SimpleAnimation(string srcStr)
+		{
+			//Creates the animation from the given source string
 
-        //Interface
+			keyframes = new List<KeyFrame>();
+			KeyFrame currentKeyframe = new KeyFrame(0, new Transformation());
 
-        public KeyFrame GetKeyframe(int index)
+			//Clean out the spaces
+			//Taken from http://stackoverflow.com/questions/6219454/efficient-way-to-remove-all-whitespace-from-string/14591148#14591148
+			string noSpaces = string.Join("", srcStr.Split(default(string[]), System.StringSplitOptions.RemoveEmptyEntries));
+
+			//Split into commands
+			string[] commands = noSpaces.Split(';');
+
+			//Execute every command
+			for (int i = 0; i < commands.Length; i++)
+			{
+				//Separate the command from the argument
+				string[] splitCommand = commands[i].Split('=');
+				string command = splitCommand[0];
+				string argument = splitCommand[1];
+
+				//Execute the command
+				if (command.Equals(OFFSET_COMMAND))
+				{
+					//Add the the keyframe to the list
+					keyframes.Add(currentKeyframe);
+
+					//Start a new keyframe with the specified offset and identical transformation to the previous one
+					float timeOffset = float.Parse(argument);
+					currentKeyframe = new KeyFrame(timeOffset, currentKeyframe.transformation);
+				}
+				else if (command.Equals(POS_COMMAND))
+				{
+					//Set the pos
+					currentKeyframe.transformation.position = Vector3FromString(argument);
+				}
+				else if (command.Equals(SCALE_COMMAND))
+				{
+					//Set the scale
+					currentKeyframe.transformation.scale = Vector3FromString(argument);
+				}
+				else if (command.Equals(EULER_ROT_COMMAND))
+				{
+					//Set the rot
+					currentKeyframe.transformation.rotation = Quaternion.Euler(Vector3FromString(argument));
+				}
+				else
+				{
+					//Throw an exception, because this is not a known command
+					throw new System.Exception("No such SimpleAnimation command");
+				}
+			}
+
+			//Add the final keyframe to the list, since the loop exits before doing that.
+			keyframes.Add(currentKeyframe);
+
+			//Calculate animation len
+			animationLen = CalculateAnimationLength();
+		}
+
+		//Interface
+
+		public KeyFrame GetKeyframe(int index)
         {
             return keyframes[index];
         }
@@ -86,6 +152,31 @@ namespace SimpleAnimationSystem
 
             return len;
         }
+
+		private Vector3 Vector3FromString(string str)
+		{
+			//Converts the given string to a vector 3
+
+			//Throw an exception if it doesn't start and end with parentheses.
+			if (!str.StartsWith("(") || !str.EndsWith(")"))
+			{
+				throw new System.Exception("Invalid SimpleAnimation Vector3 argument");
+			}
+
+			//Split into components
+			string[] components = str.Split(',');
+			string xStr = components[0];
+			string yStr = components[1];
+			string zStr = components[2];
+
+			//Remove the beginning/ending parens from the components
+			xStr = xStr.TrimStart('(');
+			zStr = zStr.TrimEnd(')');
+
+			//Parse the components
+			return new Vector3(float.Parse(xStr), float.Parse(yStr), float.Parse(zStr));
+		}
+
     }
 
     public class KeyFrame
@@ -98,5 +189,28 @@ namespace SimpleAnimationSystem
             this.timeOffset = timeOffset;
             this.transformation = transformation;
         }
+
+		public override string ToString()
+		{
+			//Returns the string representation of this keyframe
+
+			StringBuilder builder = new StringBuilder();
+
+			builder.AppendLine("\t" + SimpleAnimation.OFFSET_COMMAND + " = " + timeOffset + ";");
+			builder.Append("\t" + SimpleAnimation.POS_COMMAND + " = " + Vector3String(transformation.position) + ";");
+			builder.Append("\t" + SimpleAnimation.SCALE_COMMAND + " = " + Vector3String(transformation.scale) + ";");
+			builder.Append("\t" + SimpleAnimation.EULER_ROT_COMMAND + " = " + Vector3String(transformation.rotation.eulerAngles) + ";");
+
+			return builder.ToString();
+		}
+
+		//Misc methods
+
+		private string Vector3String(Vector3 vec)
+		{
+			//Converts a given vector3 into a string
+
+			return "(" + vec.x + ", " + vec.y + ", " + vec.z + ")";
+		}
     }
 }
