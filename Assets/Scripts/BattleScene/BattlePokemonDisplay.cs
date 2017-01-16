1076422646bb369453a2adcd57462199196618c1
Alex Shelley <ashelley@purdue.edu>
2017-01-16 18:33:58 -0500

    The tackle animation is now stored and loaded as a .txt file.

--- Assets/Scripts/BattleScene/BattlePokemonDisplay.cs
+++ Assets/Scripts/BattleScene/BattlePokemonDisplay.cs
@@ -68,21 +68,9 @@ public static class TestAnimations
 
 	static TestAnimations()
 	{
-		//Create the tackle animation
-		List<KeyFrame> frames = new List<KeyFrame>();
-		KeyFrame start = new KeyFrame(0, new Transformation(Vector3.zero, Vector3.one, Quaternion.identity));
-		KeyFrame end = new KeyFrame(0.1f, new Transformation(new Vector3(1, 1), Vector3.one, Quaternion.identity));
-		KeyFrame comeBack = new KeyFrame(0.3f, start.transformation);
-
-		frames.Add(start);
-		frames.Add(end);
-		frames.Add(comeBack);
-
-		tackle = new SimpleAnimation(frames);
-		Debug.Log(tackle.ToString());
-
-		//Create the tackle animation using its own ToString
-		tackle = new SimpleAnimation(tackle.ToString());
+		//Load the tackle animation
+		TextAsset tackleFile = Resources.Load<TextAsset>("SimpleAnimations/tackle");
+		tackle = new SimpleAnimation(tackleFile.text);
 	}
 
 }
